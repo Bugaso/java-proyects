b23e1ca41d1ffb7a84aeac4724ba90d0dd46ed25
Bugaso <beelli380@gmail.com>
2023-08-09 15:22:56 -0300

    "Unidad 5"

--- Unidad5ArreglosyCollecciones/Ejercicio3U5/src/Main.java
+++ Unidad5ArreglosyCollecciones/Ejercicio3U5/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -10,5 +13,40 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Ingrese el tamaño del vector");
+        Scanner leer = new Scanner(System.in);
+        int n = leer.nextInt();
+
+        int[] vector = new int[n];
+        int[] ubicaciones = new int[n];
+        Random aleatorio = new Random(System.currentTimeMillis());
+
+        for(int i=0;i<n;i++){
+            vector[i]= aleatorio.nextInt(0,9);
+            System.out.println("Valor del vector["+ i +"] = "+vector[i]);
+        }
+
+        System.out.println("Ingrese un numero a buscar: ");
+        n = leer.nextInt();
+        int count = 0;
+
+        for (int i = 0, j = 0; i< vector.length; i++){
+            if(n == vector[i]) {
+                ubicaciones[count]=i;
+                count ++;
+            }
+        }
+
+        if(count>=1){
+            System.out.println("El numero fue encontrado en la ubicacion vector["+ubicaciones[0]+"]");
+            if(count>1){
+                System.out.println("Y en las sig ubicaciones: ");
+                for (int i=0;i<count;i++){
+                    System.out.println("Vector ["+ubicaciones[i]+"]");
+                }
+            }else{
+                System.out.println("Y no se repite");
+            }
+        }
     }
 }
