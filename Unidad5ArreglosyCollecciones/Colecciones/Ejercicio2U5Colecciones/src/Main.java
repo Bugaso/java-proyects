78eb0757d4228baa2855f876896b409edda936e2
Bugaso <beelli380@gmail.com>
2023-08-25 11:02:19 -0300

    "Unidad 5 Ejercicios de Colecciones"

--- Unidad5ArreglosyCollecciones/Colecciones/Ejercicio2U5Colecciones/src/Main.java
+++ Unidad5ArreglosyCollecciones/Colecciones/Ejercicio2U5Colecciones/src/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         /*
@@ -6,11 +10,37 @@ public class Main {
         HashSet sepa cuando una Película está repetida, es decir, tienen el mismo título, bastará con
         sobreescribir los métodos equals y hashCode en la clase Película.
          */
-        Cine Rensi = new Cine("Rensi","Vea");
-        Pelicula p1 = new Pelicula("LEGO","Yo",1);
-        Pelicula p2 = new Pelicula("LEGO","Yo",1);
-        Rensi.agregarPelicula(p1);
-        Rensi.agregarPelicula(p2);
-        Rensi.listarTodo();
+        Cine ROMA = new Cine("ROMA SRL","Rivadavia 325");
+
+
+        System.out.println("Ingrese la cantidad de peliculas a agregar: ");
+        Scanner leer = new Scanner(System.in);
+        int cantP= leer.nextInt();
+        Pelicula[] peliculas = new Pelicula[cantP];
+        int duracion;
+        String TituloDirec;
+        String TituloPeli;
+        leer.nextLine();
+        for(int i=0;i<peliculas.length;i++){
+            System.out.println("Ingrese los datos de la pelicula "+(i+1));
+
+            System.out.println("Titulo: ");
+            TituloPeli = leer.nextLine();
+
+
+            System.out.println("Duracion: ");
+            duracion=leer.nextInt();
+
+
+            System.out.println("Director: ");
+            leer.nextLine();
+            TituloDirec=leer.nextLine();
+
+            peliculas[i] = new Pelicula(TituloPeli,TituloDirec,duracion);
+            ROMA.agregarPelicula(peliculas[i]);
+        }
+
+        ROMA.listarTodo();
+
     }
 }
